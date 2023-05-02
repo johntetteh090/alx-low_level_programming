ef80e53e3659f7770632b0a298820af08109a343
johntetteh090 <johntetteh090@gmail.com>
2023-05-02 14:43:05 -0700

    initial commit

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * find_list int_loop - function that finds the loop in a linked list
+ * @head:
+ * Return: return loop immediately
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+  head++;
+  return (0);
+}
