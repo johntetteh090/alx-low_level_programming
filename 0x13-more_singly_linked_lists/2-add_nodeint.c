1089031039c9cd4e9e54018c453f762a0a9c84a4
johntetteh090 <johntetteh090@gmail.com>
2023-05-03 03:47:43 -0700

    initial commit

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,23 +1,46 @@
 #include "lists.h"
-#include <stdlib.h>
+
+static listint_t *new_node(const int n);
+
 /**
- * add_node int - adds a new node at the beginning of a listint_t list
- * @head: current place in list
- * @n: int to add to list
- * Return: pointer current position in the list
+ * add_nodeint - add a node at the head of a list.
+ * @head : pointer of a pointer to a list.
+ * @n : value to set the new list to.
+ *
+ * Return: pointer to a pointer of listint_t
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
-	new = *head;
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (!(*head))
 	{
-		return (NULL);
+		*head = new_node(n);
+		return (*head);
 	}
-	new->n = n;
+
+	new = new_node(n);
 	new->next = *head;
 	*head = new;
 	return (*head);
 }
+
+/**
+ * new_node - creates a new listint_t node.
+ * @n: number to place inside the node.
+ *
+ * Return: pointer to the new node.
+ */
+static listint_t *new_node(const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	return (new);
+}
+
+
